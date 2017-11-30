cb1c5aca57b706491b0aa0838d92ffa3283b59e9
Jvanmourik <jacco.vanmourik@me.com>
2017-11-30 14:56:33 +0100

    Change follow and avoid speed

--- modules/agent.lua
+++ modules/agent.lua
@@ -7,27 +7,27 @@ local function agent(node)
 
   function self:follow(target)
     if(node.x > target.x) then
-      node.x = node.x - 5
+      node.x = node.x - 1
     else
-      node.x = node.x + 5
+      node.x = node.x + 1
     end
     if(node.y > target.y) then
-      node.y = node.y - 5
+      node.y = node.y - 1
     else
-      node.y = node.y + 5
+      node.y = node.y + 1
     end
   end
 
   function self:dodge(target)
     if(node.x > target.x) then
-      node.x = node.x + 5
+      node.x = node.x + 1
     else
-      node.x = node.x - 5
+      node.x = node.x - 1
     end
     if(node.y > target.y) then
-      node.y = node.y + 5
+      node.y = node.y + 1
     else
-      node.y = node.y - 5
+      node.y = node.y - 1
     end
   end
 
