4b6d5b93e62a8038c719115411b73a8f729d5fc3
Jvanmourik <jacco.vanmourik@me.com>
2017-12-04 17:19:34 +0100

    Fix vector.angle() now spans a full 360 degree range

--- lib/vector.lua
+++ lib/vector.lua
@@ -2,23 +2,27 @@ local vector = {}
 
 -- calculate the angle in radians of two vectors
 function vector.angle(x1, y1, x2, y2)
-	local d = vector.dot(x1, y1, x2, y2)
-  local l1 = vector.length(x1, y1)
-  local l2 = vector.length(x2, y2)
-  return math.acos(d / (l1 * l2))
+	local dot = vector.dot(x1, y1, x2, y2)
+  local det = vector.det(x1, y1, x2, y2)
+  return math.atan2(det, dot)
 end
 
--- calculate dot product of two vectors
+-- calculate the determinant of two vectors
+function vector.det(x1, y1, x2, y2)
+  return x1 * y2 - y1 * x2
+end
+
+-- calculate the dot product of two vectors
 function vector.dot(x1, y1, x2, y2)
   return x1 * x2 + y1 * y2
 end
 
--- calculate magnitude of vector
+-- calculate the magnitude of a vector
 function vector.length(x, y)
 	return math.sqrt(x * x + y * y)
 end
 
--- normalize vector
+-- normalize a vector
 function vector.normalize(x, y)
 	local l = vector.length(x, y)
 	if l > 0 then
