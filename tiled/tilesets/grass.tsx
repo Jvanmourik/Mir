bd9d493ffe7eac8f28ee8ad44a31ad04f9eb0478
FreakyFrek <freek_schipper@hotmail.com>
2017-12-15 12:15:28 +0100

    Testmap Tiled code

---
+++ tiled/tilesets/grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="grass-tiles-2-small.png" width="192" height="64"/>
+</tileset>
