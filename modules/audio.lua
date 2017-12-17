44cba7f75fa85cb5ad28c325ff6533059c97c4a3
Zektra <mickeyspiggy@gmail.com>
2017-12-17 15:51:49 +0100

    Add music sliders, enhance menu

--- modules/audio.lua
+++ modules/audio.lua
@@ -1,2 +1,4 @@
 bgMusic = la.newSource("assets/audio/background.mp3", "static")
 bgMusic:play()
+
+efMusic = la.newSource("assets/audio/background.mp3")
--- modules/gui.lua
+++ modules/gui.lua
@@ -5,13 +5,15 @@ local function gui()
   -- components
   ----------------------------------------------
   
-  -- load GUI related stuff
   gameState = 0
-  function love.load()
-    local font = love.graphics.newFont("NotoSansHans-Regular.otf", 20 * scale)
-    love.graphics.setFont(font)
-  end
-
+  
+  -- set font
+  local font = love.graphics.newFont(20 * scale)
+  love.graphics.setFont(font)
+  
+  -- audio sliders
+  local sliderM = {value = 1, min = 0, max = 1}
+  local sliderE = {value = 1, min = 0, max = 1}
 
   ----------------------------------------------
   -- methods
@@ -19,12 +21,32 @@ local function gui()
 
   function self:update(dt)
     suit.layout:reset(100 * scale, 100 * scale)
+    suit.layout:padding(10 * scale, 10 * scale)
+    
+    -- background music slider
+    suit.Label("Music", suit.layout:row(200 * scale, 20 * scale))
+    suit.Slider(sliderM, suit.layout:row(200 * scale, 20 * scale))
+    suit.Label(tostring(math.floor(sliderM.value * 100)).."%", suit.layout:col(200 * scale, 20 * scale))
+    bgMusic:setVolume(sliderM.value)
+    
+    suit.layout:left()
+    
+    -- effects music slider
+    suit.Label("Effects", suit.layout:row(200 * scale, 20 * scale))
+    suit.Slider(sliderE, suit.layout:row(200 * scale, 20 * scale))
+    suit.Label(tostring(math.floor(sliderE.value * 100).."%"), suit.layout:col(200 * scale, 20 * scale))
+    efMusic:setVolume(sliderE.value)
+    
+    suit.layout:left()
+    
+    -- start game
     if suit.Button("Start!", suit.layout:row(200 * scale, 30 * scale)).hit then
       gameState = 1
     end
     if lk.isDown("escape") then
       gameState = 0
     end
+    
   end
   
   ----------------------------------------------
