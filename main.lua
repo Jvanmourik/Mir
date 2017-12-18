df29b5857cc4141a5af820cb1fbd7dfed5f98a4b
FreakyFrek <freek_schipper@hotmail.com>
2017-12-18 14:06:11 +0100

    WIP

--- main.lua
+++ main.lua
@@ -4,24 +4,62 @@ local world
 local tx, ty
 
 function love.load()
+	-- declare shorthand framework names
+	lg = love.graphics
+	li = love.image
+	la = love.audio
+	lm = love.mouse
+	lk = love.keyboard
+	lt = love.timer
+	le = love.event
+	ls = love.system
+	lw = love.window
+	lf = love.filesystem
+
 	-- Load map
 	map = sti("tiled/tilesets/level.lua", { "box2d" })
-	local Character = require "modules/character"
 
-	-- Creat
-	local layer = map:addCustomLayer("Sprites", 8)
+	    -- Create new dynamic data layer called "Sprites" as the 8th layer
+	    local layer = map:addCustomLayer("Sprites", 8)
 
-	local player
-	for k, object in pairs(map.objects) do
-		if object.name == "Player" then
-				player = object
-				break
-			end
-		end
+	    -- Get player spawn object
+	    local player
+	    for k, object in pairs(map.objects) do
+	        if object.name == "playerSpawn" then
+	            player = object
+	            break
+	        end
+	    end
 
-	-- Create player object
-	layer.player = Character(player.x, player.y)
-	scene.rootNode:addChild(c)
+	    -- Create player object
+	    local sprite = love.graphics.newImage("assets/images/atlas.png")
+	    layer.player = {
+	        sprite = sprite,
+	        x      = player.x,
+	        y      = player.y,
+	        ox     = sprite:getWidth() / 2,
+	        oy     = sprite:getHeight() / 1.35
+	    }
+
+	    -- Draw player
+	    layer.draw = function(self)
+	        love.graphics.draw(
+	            self.player.sprite,
+	            math.floor(self.player.x),
+	            math.floor(self.player.y),
+	            0,
+	            1,
+	            1,
+	            self.player.ox,
+	            self.player.oy
+	        )
+
+	        -- Temporarily draw a point at our location so we know
+	        -- that our sprite is offset properly
+	        love.graphics.setPointSize(5)
+	        love.graphics.points(math.floor(self.player.x), math.floor(self.player.y))
+	    end
+
 	-- Prepare translations
 	tx, ty = 0, 0
 
@@ -29,13 +67,15 @@ function love.load()
 	--love.physics.setMeter(32)
 	--world = love.physics.newWorld(0, 0)
 	--map:box2d_init(world)
+	-- Remove unneeded object layer
+	--map:removeLayer("objectlaag")
 end
 
 function love.update(dt)
 	--world:update(dt)
 	--map:update(dt)
 
-	-- Move map
+	--Move map
 	local kd = love.keyboard.isDown
 	local l  = kd("left")  or kd("a")
 	local r  = kd("right") or kd("d")
