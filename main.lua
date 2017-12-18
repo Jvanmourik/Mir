c48ced735b96f2bed83f5c74a1b1414836073fe2
Jacco van Mourik <jacco.vanmourik@me.com>
2017-12-18 16:03:47 +0100

    Cleanup

--- main.lua
+++ main.lua
@@ -1,10 +1,5 @@
-local sti = require "lib/sti"
-local map
-local world
-local tx, ty
-
 function love.load()
-	-- declare shorthand framework names
+  -- declare shorthand framework names
 	lg = love.graphics
 	li = love.image
 	la = love.audio
@@ -16,79 +11,42 @@ function love.load()
 	lw = love.window
 	lf = love.filesystem
 
-	-- Load map
-	map = sti("tiled/tilesets/level.lua", { "box2d" })
+  -- set background color
+  lg.setBackgroundColor(222, 222, 222)
 
-	    -- Create new dynamic data layer called "Sprites" as the 8th layer
-	    local layer = map:addCustomLayer("Sprites", 5)
+  -- load extensions
+  require "extensions"
 
-	    -- Get player spawn object
-	    local player
-	    for k, object in pairs(map.objects) do
-	        if object.name == "playerSpawn" then
-	            player = object
-	            break
-	        end
-	    end
+  -- load libraries
+  vector = require "lib/vector"
 
-	    -- Create player object
-	    local sprite = love.graphics.newImage("assets/images/atlas.png")
-	    layer.player = {
-	        sprite = sprite,
-	        x      = player.x,
-	        y      = player.y,
-	        ox     = sprite:getWidth() / 2,
-	        oy     = sprite:getHeight() / 1.35
-	    }
+  -- load templates
+  --assets = require "templates/assets"
+  graphics = require "templates/graphics"
+  animations = require "templates/animations"
 
-	    -- Draw player
-	    layer.draw = function(self)
-	        love.graphics.draw(
-	            self.player.sprite,
-	            math.floor(self.player.x),
-	            math.floor(self.player.y),
-	            0,
-	            1,
-	            1,
-	            self.player.ox,
-	            self.player.oy
-	        )
+  -- load modules
+  local Scene = require "modules/scene"
+	local Tilemap = require "modules/tilemap"
+  local Character = require "modules/character"
 
-	        -- Temporarily draw a point at our location so we know
-	        -- that our sprite is offset properly
-	        love.graphics.setPointSize(5)
-	        love.graphics.points(math.floor(self.player.x), math.floor(self.player.y))
-	    end
-	print(player.x)
-	-- Prepare translations
-	tx, ty = 0, 0
+  -- create scene
+  scene = Scene(0, 0)
 
-	-- Prepare physics world
-	--love.physics.setMeter(32)
-	--world = love.physics.newWorld(0, 0)
-	--map:box2d_init(world)
-	-- Remove unneeded object layer
-	--map:removeLayer("objectlaag")
+  -- populate scene
+  local t = Tilemap()
+  scene.rootNode:addChild(t)
+
+  local c = Character(100, 100)
+  scene.rootNode:addChild(c)
 end
 
 function love.update(dt)
-	--world:update(dt)
-	--map:update(dt)
-
-	--Move map
-	local kd = love.keyboard.isDown
-	local l  = kd("left")  or kd("a")
-	local r  = kd("right") or kd("d")
-	local u  = kd("up")    or kd("w")
-	local d  = kd("down")  or kd("s")
-
-	tx = l and tx - 128 * dt or tx
-	tx = r and tx + 128 * dt or tx
-	ty = u and ty - 128 * dt or ty
-	ty = d and ty + 128 * dt or ty
+  -- update scene
+  scene:update(dt)
 end
 
 function love.draw()
-	-- Draw map
-	map:draw(-tx, -ty)
+  -- draw scene
+	scene:draw()
 end
