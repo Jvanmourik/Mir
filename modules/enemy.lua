3991a762889d1d3bbf57841d66b298a817a5574a
bibi0bla <bibi0blabibi238@gmail.com>
2018-01-23 20:49:37 +0100

    Enemy archers

--- modules/enemy.lua
+++ modules/enemy.lua
@@ -1,4 +1,7 @@
 local Character = require "modules/character"
+local Item = require "modules/item"
+local Projectile = require "modules/projectile"
+
 
 local function enemy(x, y)
   local self = Character(x, y)
@@ -12,7 +15,16 @@ local function enemy(x, y)
   self.speed = 400
   self.health = 20
   self.maxhealth = 20
+  local shootTimer = 0
 
+  local weaponRandom = love.math.random()
+  if weaponRandom < 0.3 then
+    self.weapon.id = 2
+    self.weapon.name = "regularBow"
+    self.weapon.damage = 5
+    self.weapon.type = "bow"
+  end
+  print(self.weapon.type)
   ----------------------------------------------
   -- components
   ----------------------------------------------
@@ -28,9 +40,17 @@ local function enemy(x, y)
   local target
   local players
   local timer = 0
-  local aggroDistance = 200
+  local aggroDistance
+  local minimumDistance
   local attackCooldown = 1500
   local isAttacking = false
+  if self.weapon.type == "sword" then
+    aggroDistance = 200
+    minimumDistance = 100
+  else
+    aggroDistance = 400
+    minimumDistance = 600
+  end
 
   -- update function called each frame, dt is time since last frame
   function self:update(dt)
@@ -61,7 +81,7 @@ local function enemy(x, y)
       -- if target in range
       if d < aggroDistance then
         -- move to target
-        if d > 100 and not isAttacking then
+        if d > minimumDistance and not isAttacking then
           self.agent:goToPoint(target.x, target.y)
         elseif self.agent.state == "walk" then
           self.agent:stop()
@@ -73,13 +93,13 @@ local function enemy(x, y)
         end
 
         -- attack
-        if d < 100 and timer <= 0 then
-          isAttacking = true
-          self:attack(function()
-            isAttacking = false
-          end)
-          timer = attackCooldown
-        end
+          if d < minimumDistance and timer <= 0 then
+            isAttacking = true
+            self:attack(function()
+              isAttacking = false
+            end)
+            timer = attackCooldown
+          end
       end
 
       -- update timer
@@ -90,15 +110,32 @@ local function enemy(x, y)
     base.update(self, dt)
   end
 
+  function self:attack(callback)
+    if self.weapon.type == "sword" then
+      base.attack(self, callback)
+    elseif self.weapon.type == "bow" then
+      local deltaX = target.x - self.x
+      local deltaY = target.y - self.y
+      local dirX, dirY = vector.normalize(deltaX, deltaY)
+      spawnX , spawnY = self.x + dirX * 50, self.y + dirY * 50
+      local arrow = Projectile(spawnX, spawnY, dirX, dirY, self.weapon.damage)
+      scene.rootNode:addChild(arrow)
+      -- invoke callback
+      if callback then callback() end
+    end
+  end
+
   function self:kill()
     local randomNumber = love.math.random()
     local item
-    if randomNumber <= 0.5 then
-      item = Item(1, self.x, self.y)
-    elseif randomNumber > 0.5 and randomNumber <= 0.8 then
-      item = Item(2, self.x, self.y)
+    if self.weapon.type == "sword" then
+      if randomNumber <= 0.8 then
+        item = Item(1, self.x, self.y)
+      else
+        item = Item(5, self.x, self.y)
+      end
     else
-      item = Item(5, self.x, self.y)
+      item = Item(2, self.x, self.y)
     end
     scene.rootNode:addChild(item)
     base.kill(self)
