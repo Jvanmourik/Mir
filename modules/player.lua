21b3775823492f7f5e93451e72ac0a2b25d89e07
bibi0bla <bibi0blabibi238@gmail.com>
2018-01-23 13:27:24 +0100

    With Dash

--- modules/player.lua
+++ modules/player.lua
@@ -13,8 +13,8 @@ local function character(x, y, gamepad)
   self.name = "player"
   self.health = 30
   self.maxhealth = 30
-  self.rollSpeed = 800
   local shootTimer = 0
+
 
   ----------------------------------------------
   -- methods
@@ -22,63 +22,65 @@ local function character(x, y, gamepad)
 
   -- update function called each frame, dt is time since last frame
   function self:update(dt)
+    if not self.isDashing then
+      -- direction vector
+      local dirX, dirY = 0, 0
 
-    -- direction vector
-    local dirX, dirY = 0, 0
+      if gamepad then
+        -- gamepad input
+        local leftX = gamepad:getAxis('leftx')
+        local leftY = gamepad:getAxis('lefty')
+        local rightX = gamepad:getAxis('rightx')
+        local rightY = gamepad:getAxis('righty')
 
-    if gamepad then
-      -- gamepad input
-      local leftX = gamepad:getAxis('leftx')
-      local leftY = gamepad:getAxis('lefty')
-      local rightX = gamepad:getAxis('rightx')
-      local rightY = gamepad:getAxis('righty')
+        -- normalize input
+        if vector.length(leftX, leftY) > 0.3 then
+          dirX, dirY = vector.normalize(leftX, leftY)
+        end
 
-      -- normalize input
-      if vector.length(leftX, leftY) > 0.3 then
-        dirX, dirY = vector.normalize(leftX, leftY)
+        -- make character look at direction
+        if vector.length(rightX, rightY) > 0.3 then
+          self.body.rotation = vector.angle(0, 1, vector.normalize(rightX, rightY))
+        end
+      else
+        -- keyboard input
+        if input:isDown('a') or input:isDown('left') then dirX = -1 end
+        if input:isDown('d') or input:isDown('right') then dirX = 1 end
+        if input:isDown('w') or input:isDown('up') then dirY = -1 end
+        if input:isDown('s') or input:isDown('down') then dirY = 1 end
+
+        -- normalize input
+        dirX, dirY = vector.normalize(dirX, dirY)
+
+        -- make character look at direction
+        self.body:lookAt(camera:mousePosition())
       end
 
-      -- make character look at direction
-      if vector.length(rightX, rightY) > 0.3 then
-        self.body.rotation = vector.angle(0, 1, vector.normalize(rightX, rightY))
-      end
-    else
-      -- keyboard input
-      if input:isDown('a') or input:isDown('left') then dirX = -1 end
-      if input:isDown('d') or input:isDown('right') then dirX = 1 end
-      if input:isDown('w') or input:isDown('up') then dirY = -1 end
-      if input:isDown('s') or input:isDown('down') then dirY = 1 end
+      -- is character speed below or equal to max speed?
+      if vector.length(self.velocityX, self.velocityY) <= self.speed then
+        -- apply input multiplied with speed to velocity
+        self.velocityX, self.velocityY = dirX * self.speed, dirY * self.speed
 
-      -- normalize input
-      dirX, dirY = vector.normalize(dirX, dirY)
-
-      -- make character look at direction
-      self.body:lookAt(camera:mousePosition())
-    end
-
-    -- is character speed below or equal to max speed?
-    if vector.length(self.velocityX, self.velocityY) <= self.speed then
-      -- apply input multiplied with speed to velocity
-      self.velocityX, self.velocityY = dirX * self.speed, dirY * self.speed
-
-      -- character roll
-      if vector.length(dirX, dirY) > 0.3 then
-        if input:isPressed("space") or gamepad and gamepad:isPressed('b') then
-          self.velocityX, self.velocityY = dirX * self.rollSpeed, dirY * self.rollSpeed
+        -- character dash
+        if vector.length(dirX, dirY) > 0.3 then
+          if input:isPressed("space") or gamepad and gamepad:isPressed('b') then
+            self:dash(dirX, dirY)
+          end
         end
       end
-    end
 
-    -- character attack
-    if not gamepad and input:isPressed(1) or gamepad and gamepad:isPressed('rightshoulder') then
-      self:attack()
-    end
-    -- pickup item on the ground
-    if not gamepad and input:isPressed('e') or gamepad and gamepad:isPressed('x') then
-      local item = self:pickupAnyItem()
-      if item then
-        self:throwItem(Item(self.weapon.id, self.x, self.y), 1000)
-        self:equipItem(item)
+      -- character attack
+      if not gamepad and input:isPressed(1) or gamepad and gamepad:isPressed('rightshoulder') then
+        self:attack()
+      end
+
+      -- pickup item on the ground
+      if not gamepad and input:isPressed('e') or gamepad and gamepad:isPressed('x') then
+        local item = self:pickupAnyItem()
+        if item then
+          self:throwItem(Item(self.weapon.id, self.x, self.y), 1000)
+          self:equipItem(item)
+        end
       end
     end
 
